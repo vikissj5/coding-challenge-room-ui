6c5c5229303c8ec4d871723c962876c9d9fab1bc
vikissj5 <vikissj5@gmail.com>
2024-04-06 15:45:19 +0530

    First working model

--- room-backend/src/app.js
+++ room-backend/src/app.js
@@ -9,9 +9,31 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// Function to generate a random integer between min  and max 
+function getRandomInt(min, max) {
+
+    const temp = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(temp);
+    return temp;
+}
+
+// Generate random temperature value between 10 and 40
+function getRandomTemperature() {
+    return getRandomInt(10, 40);
+}
+
+
 const setAcStatus = async (data) => {
 
     data?.building?.rooms?.forEach((item) => {
+        if (!item.temp) {
+            item.temp = getRandomTemperature();
+        }
+        if (!item.id) {
+            item.id = data?.building?.rooms?.length;
+        }
+
         if (item.temp > data?.building?.defaultTemp) {
             item.acStatus = true;
         }
@@ -20,6 +42,7 @@ const setAcStatus = async (data) => {
         }
     })
     appData = data;
+    console.log(appData.building.rooms)
 };
 
 // app.get('/building/', (req, res) => {
@@ -33,7 +56,7 @@ const setDefaultTemp = async (temp) => {
 }
 
 app.get('/building', async (req, res) => {
-    await setAcStatus(data);
+    await setAcStatus(appData);
     res.send(appData);
 });
 
@@ -47,7 +70,7 @@ app.post('/addroom', async (req, res) => {
 
 
 app.post('/setTemp', async (req, res) => {
-    await setDefaultTemp(req.body.temp);
+    await setDefaultTemp(req.body.defaultTemp);
     await setAcStatus(data);
     res.send(appData);
 })
